155a36598930a4e2ba961f44fe840ab1d4d655b3
Dirk Avery <dirk.avery@gmail.com>
2026-02-05 11:29:59 -0500

    Remove duplicate copyright headers in test data files

--- internal/service/ec2/testdata/RouteTable/list_basic/main.tf
+++ internal/service/ec2/testdata/RouteTable/list_basic/main.tf
@@ -1,9 +1,6 @@
 # Copyright IBM Corp. 2014, 2026
 # SPDX-License-Identifier: MPL-2.0
 
-# Copyright IBM Corp. 2014, 2026
-# SPDX-License-Identifier: MPL-2.0
-
 resource "aws_vpc" "test" {
   cidr_block = "10.0.0.0/16"
 
--- internal/service/ec2/testdata/RouteTable/list_basic/query.tfquery.hcl
+++ internal/service/ec2/testdata/RouteTable/list_basic/query.tfquery.hcl
@@ -1,9 +1,6 @@
 # Copyright IBM Corp. 2014, 2026
 # SPDX-License-Identifier: MPL-2.0
 
-# Copyright IBM Corp. 2014, 2026
-# SPDX-License-Identifier: MPL-2.0
-
 list "aws_route_table" "test" {
   provider = aws
 
