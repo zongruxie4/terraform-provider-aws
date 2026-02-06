0081702863a54614eff0ad99786e59ac95275217
Dirk Avery <dirk.avery@gmail.com>
2026-02-06 17:21:07 -0500

    Fix test naming convention in _list_test.go files
    Change test names from TestName_List_UpperCase to TestName_List_lowerCamelCase
    to follow Go naming standards where the final segment after underscore should
    start with lowercase.

--- internal/service/cloudwatch/metric_alarm_list_test.go
+++ internal/service/cloudwatch/metric_alarm_list_test.go
@@ -18,7 +18,7 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
-func TestAccCloudWatchMetricAlarm_List_Basic(t *testing.T) {
+func TestAccCloudWatchMetricAlarm_List_basic(t *testing.T) {
 	ctx := acctest.Context(t)
 
 	resourceName1 := "aws_cloudwatch_metric_alarm.test[0]"
