3863b5abf37abda9a2b9087a54d0b864b8cead1c
Graham Davison <gdavison@hashicorp.com>
2026-02-11 11:54:12 -0800

    Returns null when not found

--- internal/cty/path.go
+++ internal/cty/path.go
@@ -10,7 +10,7 @@ import (
 )
 
 // PathSafeApply is equivalent to cty.Path.Apply but does not return an error when one of the steps has a null or unkown value.
-// Instead, it returns the value at the point of failure and a boolean indicating whether the path was fully applied or not.
+// Instead, it returns the null value and a boolean indicating whether the path was fully applied or not.
 // Other conditions, such as invalid types or non-existent indexes, will still return an error.
 func PathSafeApply(p cty.Path, val cty.Value) (cty.Value, bool, error) {
 	var err error
@@ -22,7 +22,7 @@ func PathSafeApply(p cty.Path, val cty.Value) (cty.Value, bool, error) {
 			return cty.NilVal, false, fmt.Errorf("at step %d: %s", i, err)
 		}
 		if !HasValue(val) && i < l-1 {
-			return val, false, nil
+			return cty.NilVal, false, nil
 		}
 	}
 	return val, true, nil
