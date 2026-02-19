ff91336e925b4df4b4ae07728f53232f8212a4c4
Jared Baker <jared.baker@hashicorp.com>
2026-02-19 14:14:02 -0500

    r/aws_ec2_secondary_subnet(test): add `_List_filtered` test
    ```console
    % make t K=ec2 T=TestAccEC2SecondarySubnet_List_filtered
    make: Verifying source code with gofmt...
    ==> Checking that code complies with gofmt requirements...
    make: Running acceptance tests on branch: 🌿 f-ec2_secondary_network-list 🌿...
    TF_ACC=1 go1.25.7 test ./internal/service/ec2/... -v -count 1 -parallel 20 -run='TestAccEC2SecondarySubnet_List_filtered'  -timeout 360m -vet=off
    2026/02/19 14:05:12 Creating Terraform AWS Provider (SDKv2-style)...
    2026/02/19 14:05:12 Initializing Terraform AWS Provider (SDKv2-style)...
    
    --- PASS: TestAccEC2SecondarySubnet_List_filtered (39.73s)
    PASS
    ok      github.com/hashicorp/terraform-provider-aws/internal/service/ec2        46.911s
    ```

--- internal/service/ec2/ec2_secondary_subnet_list.go
+++ internal/service/ec2/ec2_secondary_subnet_list.go
@@ -12,9 +12,13 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	awstypes "github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	"github.com/hashicorp/terraform-plugin-framework/list"
+	listschema "github.com/hashicorp/terraform-plugin-framework/list/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/fwdiag"
 	"github.com/hashicorp/terraform-provider-aws/internal/framework"
 	fwflex "github.com/hashicorp/terraform-provider-aws/internal/framework/flex"
+	fwtypes "github.com/hashicorp/terraform-provider-aws/internal/framework/types"
+	"github.com/hashicorp/terraform-provider-aws/names"
 )
 
 // @FrameworkListResource("aws_ec2_secondary_subnet")
@@ -29,6 +33,33 @@ type secondarySubnetListResource struct {
 	framework.WithList
 }
 
+type listSecondarySubnetModel struct {
+	framework.WithRegionModel
+	Filters customListFilters `tfsdk:"filter"`
+}
+
+func (l *secondarySubnetListResource) ListResourceConfigSchema(ctx context.Context, request list.ListResourceSchemaRequest, response *list.ListResourceSchemaResponse) {
+	response.Schema = listschema.Schema{
+		Blocks: map[string]listschema.Block{
+			names.AttrFilter: listschema.ListNestedBlock{
+				CustomType: fwtypes.NewListNestedObjectTypeOf[customListFilterModel](ctx),
+				NestedObject: listschema.NestedBlockObject{
+					Attributes: map[string]listschema.Attribute{
+						names.AttrName: listschema.StringAttribute{
+							Required: true,
+						},
+						names.AttrValues: listschema.ListAttribute{
+							CustomType:  fwtypes.ListOfStringType,
+							ElementType: types.StringType,
+							Required:    true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
 func (r *secondarySubnetListResource) List(ctx context.Context, request list.ListRequest, stream *list.ListResultsStream) {
 	conn := r.Meta().EC2Client(ctx)
 
@@ -40,9 +71,14 @@ func (r *secondarySubnetListResource) List(ctx context.Context, request list.Lis
 		}
 	}
 
+	var input ec2.DescribeSecondarySubnetsInput
+	if diags := fwflex.Expand(ctx, query, &input); diags.HasError() {
+		stream.Results = list.ListResultsStreamDiagnostics(diags)
+		return
+	}
+
 	stream.Results = func(yield func(list.ListResult) bool) {
 		result := request.NewListResult(ctx)
-		var input ec2.DescribeSecondarySubnetsInput
 		for item, err := range listSecondarySubnets(ctx, conn, &input) {
 			if err != nil {
 				result = fwdiag.NewListResultErrorDiagnostic(err)
@@ -76,17 +112,13 @@ func (r *secondarySubnetListResource) List(ctx context.Context, request list.Lis
 	}
 }
 
-type listSecondarySubnetModel struct {
-	framework.WithRegionModel
-}
-
 func listSecondarySubnets(ctx context.Context, conn *ec2.Client, input *ec2.DescribeSecondarySubnetsInput) iter.Seq2[awstypes.SecondarySubnet, error] {
 	return func(yield func(awstypes.SecondarySubnet, error) bool) {
 		pages := ec2.NewDescribeSecondarySubnetsPaginator(conn, input)
 		for pages.HasMorePages() {
 			page, err := pages.NextPage(ctx)
 			if err != nil {
-				yield(awstypes.SecondarySubnet{}, fmt.Errorf("listing EC2 (Elastic Compute Cloud) Secondary Subnet resources: %w", err))
+				yield(awstypes.SecondarySubnet{}, fmt.Errorf("listing EC2 Secondary Subnet resources: %w", err))
 				return
 			}
 
---
+++ internal/service/ec2/ec2_secondary_subnet_list_test.go
@@ -0,0 +1,149 @@
+// Copyright IBM Corp. 2014, 2026
+// SPDX-License-Identifier: MPL-2.0
+
+package ec2_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/config"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/querycheck"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+	"github.com/hashicorp/terraform-plugin-testing/tfversion"
+	"github.com/hashicorp/terraform-provider-aws/internal/acctest"
+	tfknownvalue "github.com/hashicorp/terraform-provider-aws/internal/acctest/knownvalue"
+	tfquerycheck "github.com/hashicorp/terraform-provider-aws/internal/acctest/querycheck"
+	tfstatecheck "github.com/hashicorp/terraform-provider-aws/internal/acctest/statecheck"
+	"github.com/hashicorp/terraform-provider-aws/names"
+)
+
+func TestAccEC2SecondarySubnet_List_basic(t *testing.T) {
+	ctx := acctest.Context(t)
+
+	resourceName1 := "aws_ec2_secondary_subnet.test[0]"
+	resourceName2 := "aws_ec2_secondary_subnet.test[1]"
+
+	identity1 := tfstatecheck.Identity()
+	identity2 := tfstatecheck.Identity()
+
+	acctest.ParallelTest(ctx, t, resource.TestCase{
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.SkipBelow(tfversion.Version1_14_0),
+		},
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			testAccPreCheckSecondaryNetwork(ctx, t)
+		},
+		ErrorCheck:   acctest.ErrorCheck(t, names.EC2ServiceID),
+		CheckDestroy: testAccCheckSecondarySubnetDestroy(ctx),
+		Steps: []resource.TestStep{
+			// Step 1: Setup
+			{
+				ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+				ConfigDirectory:          config.StaticDirectory("testdata/SecondarySubnet/list_basic/"),
+				ConfigVariables: config.Variables{
+					"resource_count": config.IntegerVariable(2),
+				},
+				ConfigStateChecks: []statecheck.StateCheck{
+					identity1.GetIdentity(resourceName1),
+					tfstatecheck.ExpectRegionalARNFormat(resourceName1, tfjsonpath.New(names.AttrARN), "ec2", "secondary-subnet/{id}"),
+
+					identity2.GetIdentity(resourceName2),
+					tfstatecheck.ExpectRegionalARNFormat(resourceName2, tfjsonpath.New(names.AttrARN), "ec2", "secondary-subnet/{id}"),
+				},
+			},
+
+			// Step 2: Query
+			{
+				Query:                    true,
+				ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+				ConfigDirectory:          config.StaticDirectory("testdata/SecondarySubnet/list_basic/"),
+				ConfigVariables: config.Variables{
+					"resource_count": config.IntegerVariable(2),
+				},
+				QueryResultChecks: []querycheck.QueryResultCheck{
+					tfquerycheck.ExpectIdentityFunc("aws_ec2_secondary_subnet.test", identity1.Checks()),
+					tfquerycheck.ExpectIdentityFunc("aws_ec2_secondary_subnet.test", identity2.Checks()),
+				},
+			},
+		},
+	})
+}
+
+func TestAccEC2SecondarySubnet_List_filtered(t *testing.T) {
+	ctx := acctest.Context(t)
+
+	resourceNameExpected1 := "aws_ec2_secondary_subnet.expected[0]"
+	resourceNameExpected2 := "aws_ec2_secondary_subnet.expected[1]"
+	resourceNameNotExpected1 := "aws_ec2_secondary_subnet.not_expected[0]"
+	resourceNameNotExpected2 := "aws_ec2_secondary_subnet.not_expected[1]"
+
+	expected1 := tfstatecheck.Identity()
+	expected2 := tfstatecheck.Identity()
+	notExpected1 := tfstatecheck.StateValue()
+	notExpected2 := tfstatecheck.StateValue()
+
+	acctest.ParallelTest(ctx, t, resource.TestCase{
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.SkipBelow(tfversion.Version1_14_0),
+		},
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			testAccPreCheckSecondaryNetwork(ctx, t)
+		},
+		ErrorCheck:   acctest.ErrorCheck(t, names.EC2ServiceID),
+		CheckDestroy: testAccCheckSecondarySubnetDestroy(ctx),
+		Steps: []resource.TestStep{
+			// Step 1: Setup
+			{
+				ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+				ConfigDirectory:          config.StaticDirectory("testdata/SecondarySubnet/list_filtered/"),
+				ConfigVariables: config.Variables{
+					"resource_count": config.IntegerVariable(2),
+				},
+				ConfigStateChecks: []statecheck.StateCheck{
+					expected1.GetIdentity(resourceNameExpected1),
+					tfstatecheck.ExpectRegionalARNFormat(resourceNameExpected1, tfjsonpath.New(names.AttrARN), "ec2", "secondary-subnet/{id}"),
+
+					expected2.GetIdentity(resourceNameExpected2),
+					tfstatecheck.ExpectRegionalARNFormat(resourceNameExpected2, tfjsonpath.New(names.AttrARN), "ec2", "secondary-subnet/{id}"),
+
+					notExpected1.GetStateValue(resourceNameNotExpected1, tfjsonpath.New(names.AttrID)),
+					tfstatecheck.ExpectRegionalARNFormat(resourceNameNotExpected1, tfjsonpath.New(names.AttrARN), "ec2", "secondary-subnet/{id}"),
+
+					notExpected2.GetStateValue(resourceNameNotExpected2, tfjsonpath.New(names.AttrID)),
+					tfstatecheck.ExpectRegionalARNFormat(resourceNameNotExpected2, tfjsonpath.New(names.AttrARN), "ec2", "secondary-subnet/{id}"),
+				},
+			},
+
+			// Step 2: Query
+			{
+				Query:                    true,
+				ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+				ConfigDirectory:          config.StaticDirectory("testdata/SecondarySubnet/list_filtered/"),
+				ConfigVariables: config.Variables{
+					"resource_count": config.IntegerVariable(2),
+				},
+				QueryResultChecks: []querycheck.QueryResultCheck{
+					tfquerycheck.ExpectIdentityFunc("aws_ec2_secondary_subnet.test", expected1.Checks()),
+					tfquerycheck.ExpectIdentityFunc("aws_ec2_secondary_subnet.test", expected2.Checks()),
+
+					querycheck.ExpectNoIdentity("aws_ec2_secondary_subnet.test", map[string]knownvalue.Check{
+						names.AttrAccountID: tfknownvalue.AccountID(),
+						names.AttrRegion:    knownvalue.StringExact(acctest.Region()),
+						names.AttrID:        notExpected1.Value(),
+					}),
+
+					querycheck.ExpectNoIdentity("aws_ec2_secondary_subnet.test", map[string]knownvalue.Check{
+						names.AttrAccountID: tfknownvalue.AccountID(),
+						names.AttrRegion:    knownvalue.StringExact(acctest.Region()),
+						names.AttrID:        notExpected2.Value(),
+					}),
+				},
+			},
+		},
+	})
+}
