003dcc53950c5472165d9feb0e7719e366dba3b2
Dan Seiders <dan.seiders@hashicorp.com>
2026-04-02 14:24:30 -0400

    feat: fully baked ebs_volume_copy.go file

--- internal/service/ec2/ebs_volume_copy.go
+++ internal/service/ec2/ebs_volume_copy.go
@@ -15,6 +15,7 @@ import (
 	awstypes "github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	"github.com/hashicorp/terraform-plugin-framework-timeouts/resource/timeouts"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int32planmodifier"
@@ -53,9 +54,14 @@ type ebsVolumeCopyResource struct {
 func (r *ebsVolumeCopyResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
 	resp.Schema = schema.Schema{
 		Attributes: map[string]schema.Attribute{
-			names.AttrARN:              framework.ARNAttributeComputedOnly(),
-			names.AttrID:               framework.IDAttribute(),
-			names.AttrAvailabilityZone: framework.ARNAttributeComputedOnly(),
+			names.AttrARN: framework.ARNAttributeComputedOnly(),
+			names.AttrID:  framework.IDAttribute(),
+			names.AttrAvailabilityZone: schema.StringAttribute{
+				Computed: true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
 			names.AttrIOPS: schema.Int32Attribute{
 				Computed: true,
 				Optional: true,
@@ -87,9 +93,11 @@ func (r *ebsVolumeCopyResource) Schema(ctx context.Context, req resource.SchemaR
 				},
 			},
 			"source_volume_id": schema.StringAttribute{
-				Required: true,
+				Computed: true,
+				Optional: true,
 				PlanModifiers: []planmodifier.String{
 					stringplanmodifier.RequiresReplace(),
+					stringplanmodifier.UseStateForUnknown(),
 				},
 			},
 		},
@@ -111,6 +119,14 @@ func (r *ebsVolumeCopyResource) Create(ctx context.Context, req resource.CreateR
 	if resp.Diagnostics.HasError() {
 		return
 	}
+	if plan.SourceVolumeID.IsUnknown() || plan.SourceVolumeID.IsNull() || plan.SourceVolumeID.ValueString() == "" {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("source_volume_id"),
+			"Missing Source Volume ID",
+			"`source_volume_id` must be set when creating aws_ebs_volume_copy.",
+		)
+		return
+	}
 
 	var input ec2.CopyVolumesInput
 	smerr.AddEnrich(ctx, &resp.Diagnostics, flex.Expand(ctx, plan, &input, flex.WithFieldNamePrefix("EBSVolumeCopy")))
@@ -124,31 +140,29 @@ func (r *ebsVolumeCopyResource) Create(ctx context.Context, req resource.CreateR
 		smerr.AddError(ctx, &resp.Diagnostics, err, smerr.ID, "Copy of "+plan.SourceVolumeID.String())
 		return
 	}
-	if out == nil || out.Volumes == nil {
+	if out == nil || len(out.Volumes) == 0 {
 		smerr.AddError(ctx, &resp.Diagnostics, errors.New("empty output"), smerr.ID, "Copy of "+plan.SourceVolumeID.String())
 		return
 	}
-
-	smerr.AddEnrich(ctx, &resp.Diagnostics, flex.Flatten(ctx, out, &plan))
-	if resp.Diagnostics.HasError() {
+	if out.Volumes[0].VolumeId == nil {
+		smerr.AddError(ctx, &resp.Diagnostics, errors.New("empty volume ID"), smerr.ID, "Copy of "+plan.SourceVolumeID.String())
 		return
 	}
 
 	createTimeout := r.CreateTimeout(ctx, plan.Timeouts)
-	_, err = waitVolumeCreated(ctx, conn, *out.Volumes[0].VolumeId, createTimeout)
+	volume, err := waitVolumeCreated(ctx, conn, *out.Volumes[0].VolumeId, createTimeout)
 	if err != nil {
 		smerr.AddError(ctx, &resp.Diagnostics, err, smerr.ID, *out.Volumes[0].VolumeId)
 		return
 	}
 
-	volume := out.Volumes[0]
-
-	smerr.AddEnrich(ctx, &resp.Diagnostics, r.flatten(ctx, &volume, &plan))
+	smerr.AddEnrich(ctx, &resp.Diagnostics, r.flatten(ctx, volume, &plan))
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
 	plan.ID = types.StringPointerValue(volume.VolumeId)
+	plan.SourceVolumeID = types.StringPointerValue(volume.SourceVolumeId)
 	plan.ARN = fwflex.StringValueToFramework(ctx, r.Meta().RegionalARN(ctx, names.EC2, "volume/"+aws.ToString(volume.VolumeId)))
 
 	smerr.AddEnrich(ctx, &resp.Diagnostics, resp.State.Set(ctx, &plan))
@@ -179,6 +193,9 @@ func (r *ebsVolumeCopyResource) Read(ctx context.Context, req resource.ReadReque
 	if resp.Diagnostics.HasError() {
 		return
 	}
+
+	state.ARN = fwflex.StringValueToFramework(ctx, r.Meta().RegionalARN(ctx, names.EC2, "volume/"+aws.ToString(out.VolumeId)))
+	state.SourceVolumeID = types.StringPointerValue(out.SourceVolumeId)
 	setTagsOut(ctx, out.Tags)
 
 	smerr.AddEnrich(ctx, &resp.Diagnostics, resp.State.Set(ctx, &state))
@@ -192,7 +209,6 @@ func (r *ebsVolumeCopyResource) flatten(ctx context.Context, ebsVolumeCopy *awst
 func (r *ebsVolumeCopyResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
 	conn := r.Meta().EC2Client(ctx)
 
-	// TIP: -- 2. Fetch the plan
 	var plan, state ebsVolumeCopyResourceModel
 	smerr.AddEnrich(ctx, &resp.Diagnostics, req.Plan.Get(ctx, &plan))
 	smerr.AddEnrich(ctx, &resp.Diagnostics, req.State.Get(ctx, &state))
@@ -200,7 +216,6 @@ func (r *ebsVolumeCopyResource) Update(ctx context.Context, req resource.UpdateR
 		return
 	}
 
-	// TIP: -- 3. Get the difference between the plan and state, if any
 	_, d := flex.Diff(ctx, plan, state)
 	smerr.AddEnrich(ctx, &resp.Diagnostics, d)
 	smerr.AddEnrich(ctx, &resp.Diagnostics, resp.State.Set(ctx, plan))
@@ -227,17 +242,21 @@ func (r *ebsVolumeCopyResource) Update(ctx context.Context, req resource.UpdateR
 			return
 		}
 
-		smerr.AddEnrich(ctx, &resp.Diagnostics, flex.Flatten(ctx, out, &plan))
-		if resp.Diagnostics.HasError() {
-			return
-		}
-
 		updateTimeout := r.UpdateTimeout(ctx, plan.Timeouts)
-		_, err = waitVolumeUpdated(ctx, conn, plan.ID.ValueString(), updateTimeout)
+		volume, err := waitVolumeUpdated(ctx, conn, plan.ID.ValueString(), updateTimeout)
 		if err != nil {
 			smerr.AddError(ctx, &resp.Diagnostics, err, smerr.ID, plan.ID.String())
 			return
 		}
+
+		smerr.AddEnrich(ctx, &resp.Diagnostics, r.flatten(ctx, volume, &plan))
+		if resp.Diagnostics.HasError() {
+			return
+		}
+
+		plan.ID = types.StringPointerValue(volume.VolumeId)
+		plan.SourceVolumeID = types.StringPointerValue(volume.SourceVolumeId)
+		plan.ARN = fwflex.StringValueToFramework(ctx, r.Meta().RegionalARN(ctx, names.EC2, "volume/"+aws.ToString(volume.VolumeId)))
 	}
 
 	smerr.AddEnrich(ctx, &resp.Diagnostics, resp.State.Set(ctx, &plan))
@@ -267,110 +286,13 @@ func (r *ebsVolumeCopyResource) Delete(ctx context.Context, req resource.DeleteR
 	}
 
 	deleteTimeout := r.DeleteTimeout(ctx, state.Timeouts)
-	_, err = waitEBSVolumeCopyDeleted(ctx, conn, state.ID.ValueString(), deleteTimeout)
+	_, err = waitVolumeDeleted(ctx, conn, state.ID.ValueString(), deleteTimeout)
 	if err != nil {
 		smerr.AddError(ctx, &resp.Diagnostics, err, smerr.ID, state.ID.String())
 		return
 	}
 }
 
-// TIP: ==== TERRAFORM IMPORTING ====
-// The built-in import function, and Import ID Handler, if any, should handle populating the required
-// attributes from the Import ID or Resource Identity.
-// In some cases, additional attributes must be set when importing.
-// Adding a custom ImportState function can handle those.
-//
-// See more:
-// https://hashicorp.github.io/terraform-provider-aws/add-resource-identity-support/
-// func (r *ebsVolumeCopyResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
-// 	r.WithImportByIdentity.ImportState(ctx, req, resp)
-//
-// 	// Set needed attribute values here
-// }
-
-// TIP: ==== STATUS CONSTANTS ====
-// Create constants for states and statuses if the service does not
-// already have suitable constants. We prefer that you use the constants
-// provided in the service if available (e.g., awstypes.StatusInProgress).
-const (
-	statusChangePending = "Pending"
-	statusDeleting      = "Deleting"
-	statusNormal        = "Normal"
-	statusUpdated       = "Updated"
-)
-
-// func waitEBSVolumeCopyUpdated(ctx context.Context, conn *ec2.Client, id string, timeout time.Duration) (*awstypes.Volume, error) {
-// 	stateConf := &retry.StateChangeConf{
-// 		Pending:                   []string{statusChangePending},
-// 		Target:                    []string{statusUpdated},
-// 		Refresh:                   statusEBSVolumeCopy(conn, id),
-// 		Timeout:                   timeout,
-// 		NotFoundChecks:            20,
-// 		ContinuousTargetOccurence: 2,
-// 	}
-
-// 	outputRaw, err := stateConf.WaitForStateContext(ctx)
-// 	if out, ok := outputRaw.(*awstypes.Volume); ok {
-// 		return out, smarterr.NewError(err)
-// 	}
-
-// 	return nil, smarterr.NewError(err)
-// }
-
-func waitEBSVolumeCopyDeleted(ctx context.Context, conn *ec2.Client, id string, timeout time.Duration) (*awstypes.Volume, error) {
-	return waitVolumeDeleted(ctx, conn, id, timeout)
-}
-
-// func statusEBSVolumeCopy(conn *ec2.Client, id string) retry.StateRefreshFunc {
-// 	return func(ctx context.Context) (any, string, error) {
-// 		out, err := findEBSVolumeByID(ctx, conn, id)
-// 		if retry.NotFound(err) {
-// 			return nil, "", nil
-// 		}
-
-// 		if err != nil {
-// 			return nil, "", smarterr.NewError(err)
-// 		}
-
-// 		return out, aws.ToString((*string)(&out.State)), nil
-// 	}
-// }
-
-// func findEBSVolumeCopyByID(ctx context.Context, conn *ec2.Client, id string) (*awstypes.Volume, error) {
-// 	input := ec2.DescribeVolumesInput{
-// 		VolumeIds: []string{id},
-// 	}
-
-// 	out, err := conn.DescribeVolumes(ctx, &input)
-// 	if err != nil {
-// 		if retry.NotFound(err) {
-// 			return nil, smarterr.NewError(&retry.NotFoundError{
-// 				LastError: err,
-// 			})
-// 		}
-
-// 		return nil, smarterr.NewError(err)
-// 	}
-
-// 	if out == nil || out.Volumes == nil {
-// 		return nil, smarterr.NewError(tfresource.NewEmptyResultError())
-// 	}
-
-// 	return &out.Volumes[0], nil
-// }
-
-// TIP: ==== DATA STRUCTURES ====
-// With Terraform Plugin-Framework configurations are deserialized into
-// Go types, providing type safety without the need for type assertions.
-// These structs should match the schema definition exactly, and the `tfsdk`
-// tag value should match the attribute name.
-//
-// Nested objects are represented in their own data struct. These will
-// also have a corresponding attribute type mapping for use inside flex
-// functions.
-//
-// See more:
-// https://developer.hashicorp.com/terraform/plugin/framework/handling-data/accessing-values
 type ebsVolumeCopyResourceModel struct {
 	framework.WithRegionModel
 	ARN              types.String   `tfsdk:"arn"`
@@ -385,41 +307,6 @@ type ebsVolumeCopyResourceModel struct {
 	Timeouts         timeouts.Value `tfsdk:"timeouts"`
 	VolumeType       types.String   `tfsdk:"volume_type"`
 }
-
-// type complexArgumentModel struct {
-// 	NestedRequired types.String `tfsdk:"nested_required"`
-// 	NestedOptional types.String `tfsdk:"nested_optional"`
-// }
-
-// TIP: ==== IMPORT ID HANDLER ====
-// When a resource type has a Resource Identity with multiple attributes, it needs a handler to
-// parse the Import ID used for the `terraform import` command or an `import` block with the `id` parameter.
-//
-// The parser takes the string value of the Import ID and returns:
-// * A string value that is typically ignored. See documentation for more details.
-// * A map of the resource attributes derived from the Import ID.
-// * An error value if there are parsing errors.
-//
-// For more information, see https://hashicorp.github.io/terraform-provider-aws/resource-identity/#plugin-framework
-// var (
-// 	_ inttypes.ImportIDParser = ebsVolumeCopyImportID{}
-// )
-
-// type ebsVolumeCopyImportID struct{}
-
-// func (ebsVolumeCopyImportID) Parse(id string) (string, map[string]string, error) {
-// 	someValue, anotherValue, found := strings.Cut(id, intflex.ResourceIdSeparator)
-// 	if !found {
-// 		return "", nil, fmt.Errorf("id \"%s\" should be in the format <some-value>"+intflex.ResourceIdSeparator+"<another-value>", id)
-// 	}
-
-// 	result := map[string]string{
-// 		"some-value":    someValue,
-// 		"another-value": anotherValue,
-// 	}
-
-// 	return id, result, nil
-// }
 
 // TIP: ==== SWEEPERS ====
 // When acceptance testing resources, interrupted or failed tests may
