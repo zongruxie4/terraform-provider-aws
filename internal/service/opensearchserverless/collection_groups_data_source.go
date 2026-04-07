7f67b86c1e77557bf2abf43526bcc72b729aae8c
Adrian Johnson <adrian.johnson@hashicorp.com>
2026-04-07 15:07:10 -0500

    aws_opensearchserverless_collection_groups: handle multiple group summaires

--- internal/service/opensearchserverless/collection_groups_data_source.go
+++ internal/service/opensearchserverless/collection_groups_data_source.go
@@ -7,24 +7,22 @@ import (
 	"context"
 
 	"github.com/aws/aws-sdk-go-v2/service/opensearchserverless"
+	awstypes "github.com/aws/aws-sdk-go-v2/service/opensearchserverless/types"
+	"github.com/hashicorp/terraform-plugin-framework-timetypes/timetypes"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/types"
-	"github.com/hashicorp/terraform-provider-aws/internal/create"
+	"github.com/hashicorp/terraform-provider-aws/internal/flex"
 	"github.com/hashicorp/terraform-provider-aws/internal/framework"
-	"github.com/hashicorp/terraform-provider-aws/internal/framework/flex"
+	fwflex "github.com/hashicorp/terraform-provider-aws/internal/framework/flex"
 	fwtypes "github.com/hashicorp/terraform-provider-aws/internal/framework/types"
-	"github.com/hashicorp/terraform-provider-aws/names"
+	"github.com/hashicorp/terraform-provider-aws/internal/smerr"
 )
 
 // @FrameworkDataSource("aws_opensearchserverless_collection_groups", name="Collection Groups")
 func newCollectionGroupsDataSource(context.Context) (datasource.DataSourceWithConfigure, error) {
 	return &collectionGroupsDataSource{}, nil
 }
-
-const (
-	DSNameCollectionGroups = "Collection Groups Data Source"
-)
 
 type collectionGroupsDataSource struct {
 	framework.DataSourceWithModel[collectionGroupsDataSourceModel]
@@ -43,32 +41,42 @@ func (d *collectionGroupsDataSource) Schema(ctx context.Context, _ datasource.Sc
 	}
 }
 
-func (d *collectionGroupsDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+func (d *collectionGroupsDataSource) Read(ctx context.Context, request datasource.ReadRequest, response *datasource.ReadResponse) {
 	conn := d.Meta().OpenSearchServerlessClient(ctx)
 
 	var data collectionGroupsDataSourceModel
-	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
-	if resp.Diagnostics.HasError() {
+	smerr.AddEnrich(ctx, &response.Diagnostics, request.Config.Get(ctx, &data))
+	if response.Diagnostics.HasError() {
 		return
 	}
 
 	input := &opensearchserverless.ListCollectionGroupsInput{}
+	var output []summariesData
+	for item, err := range listCollectionGroups(ctx, conn, input) {
+		if err != nil {
+			smerr.AddError(ctx, &response.Diagnostics, err)
+			return
+		}
 
-	output, err := conn.ListCollectionGroups(ctx, input)
-	if err != nil {
-		resp.Diagnostics.AddError(
-			create.ProblemStandardMessage(names.OpenSearchServerless, create.ErrActionReading, DSNameCollectionGroups, "", err),
-			err.Error(),
-		)
+		s := summariesData{
+			item,
+			flex.Int64ToRFC3339StringValue(item.CreatedDate),
+		}
+		output = append(output, s)
+	}
+
+	da := struct {
+		CollectionGroupSummaries []summariesData `json:"collection_group_summaries"`
+	}{
+		CollectionGroupSummaries: output,
+	}
+
+	smerr.AddEnrich(ctx, &response.Diagnostics, fwflex.Flatten(ctx, da, &data))
+	if response.Diagnostics.HasError() {
 		return
 	}
 
-	resp.Diagnostics.Append(flex.Flatten(ctx, output, &data)...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+	smerr.AddEnrich(ctx, &response.Diagnostics, response.State.Set(ctx, &data))
 }
 
 type collectionGroupsDataSourceModel struct {
@@ -77,11 +85,16 @@ type collectionGroupsDataSourceModel struct {
 }
 
 type collectionGroupSummaryModel struct {
-	ARN                 types.String                               `tfsdk:"arn"`
-	CapacityLimits      fwtypes.ObjectValueOf[capacityLimitsModel] `tfsdk:"capacity_limits"`
-	CreatedDate         types.Int64                                `tfsdk:"created_date"`
-	ID                  types.String                               `tfsdk:"id"`
-	Name                types.String                               `tfsdk:"name"`
-	NumberOfCollections types.Int64                                `tfsdk:"number_of_collections"`
-	StandbyReplicas     types.String                               `tfsdk:"standby_replicas"`
+	ARN                 types.String                                                   `tfsdk:"arn"`
+	CapacityLimits      fwtypes.ListNestedObjectValueOf[capacityLimitsDataSourceModel] `tfsdk:"capacity_limits"`
+	CreatedDate         timetypes.RFC3339                                              `tfsdk:"created_date"`
+	ID                  types.String                                                   `tfsdk:"id"`
+	Name                types.String                                                   `tfsdk:"name"`
+	NumberOfCollections types.Int32                                                    `tfsdk:"number_of_collections"`
+	StandbyReplicas     types.String                                                   `tfsdk:"standby_replicas"`
+}
+
+type summariesData struct {
+	awstypes.CollectionGroupSummary
+	CreatedDate string `tfsdk:"created_date"`
 }
