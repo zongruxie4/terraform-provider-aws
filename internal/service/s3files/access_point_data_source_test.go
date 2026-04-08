72a60a718733712b7cf55b5fa4a26f4200cae081
Dirk Avery <dirk.avery@gmail.com>
2026-04-08 10:53:18 -0400

    Update IAM policies

--- internal/service/s3files/access_point_data_source_test.go
+++ internal/service/s3files/access_point_data_source_test.go
@@ -43,66 +43,9 @@ func TestAccS3FilesAccessPointDataSource_basic(t *testing.T) {
 }
 
 func testAccAccessPointDataSourceConfig_basic(rName string) string {
-	return fmt.Sprintf(`
-resource "aws_s3_bucket" "test" {
-  bucket = "s3files-private-beta-2025-%[1]s"
-}
-
-resource "aws_s3_bucket_versioning" "test" {
-  bucket = aws_s3_bucket.test.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_iam_role" "test" {
-  name = %[1]q
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "elasticfilesystem.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy" "test" {
-  name = %[1]q
-  role = aws_iam_role.test.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:DeleteObject",
-          "s3:ListBucket"
-        ]
-        Resource = [
-          aws_s3_bucket.test.arn,
-          "${aws_s3_bucket.test.arn}/*"
-        ]
-      }
-    ]
-  })
-}
-
-resource "aws_s3files_file_system" "test" {
-  bucket   = aws_s3_bucket.test.arn
-  role_arn = aws_iam_role.test.arn
-
-  depends_on = [aws_s3_bucket_versioning.test]
-}
-
+	return acctest.ConfigCompose(
+		testAccAccessPointConfig_base(rName),
+		fmt.Sprintf(`
 resource "aws_s3files_access_point" "test" {
   file_system_id = aws_s3files_file_system.test.id
 
@@ -119,5 +62,5 @@ resource "aws_s3files_access_point" "test" {
 data "aws_s3files_access_point" "test" {
   id = aws_s3files_access_point.test.id
 }
-`, rName)
+`, rName))
 }
