426937dd721c708b9886eb4909f3fcc84b9767f0
Kit Ewbank <Kit_Ewbank@hotmail.com>
2026-04-10 15:44:23 -0400

    r/aws_autoscaling_lifecycle_hook: Add Resource Identity support.

--- internal/service/autoscaling/service_package_gen.go
+++ internal/service/autoscaling/service_package_gen.go
@@ -80,6 +80,14 @@ func (p *servicePackage) SDKResources(ctx context.Context) []*inttypes.ServicePa
 			TypeName: "aws_autoscaling_lifecycle_hook",
 			Name:     "Lifecycle Hook",
 			Region:   unique.Make(inttypes.ResourceRegionDefault()),
+			Identity: inttypes.RegionalParameterizedIdentity([]inttypes.IdentityAttribute{
+				inttypes.StringIdentityAttribute("autoscaling_group_name", true),
+				inttypes.StringIdentityAttribute(names.AttrName, true),
+			}),
+			Import: inttypes.SDKv2Import{
+				WrappedImport: true,
+				ImportID:      lifecycleHookImportID{},
+			},
 		},
 		{
 			Factory:  resourceNotification,
