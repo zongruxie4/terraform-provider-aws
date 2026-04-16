4d36db3dd9b8174632a9be036d179c0b257a3cfd
Jared Baker <jared.baker@hashicorp.com>
2026-04-16 13:36:58 -0400

    r/aws_ebs_volume_copy: re-generate service package post merge

--- internal/service/ec2/service_package_gen.go
+++ internal/service/ec2/service_package_gen.go
@@ -103,7 +103,7 @@ func (p *servicePackage) FrameworkResources(ctx context.Context) []*inttypes.Ser
 			Tags: unique.Make(inttypes.ServicePackageResourceTags{
 				IdentifierAttribute: names.AttrID,
 			}),
-			Region: unique.Make(inttypes.ResourceRegionDefault()),
+			Region: inttypes.ResourceRegionDefault(),
 		},
 		{
 			Factory:  newAllowedImagesSettingsResource,
