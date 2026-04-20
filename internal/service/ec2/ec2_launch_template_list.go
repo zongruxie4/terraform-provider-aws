903259c086e184e41d1856679b10301acf764049
Dirk Avery <dirk.avery@gmail.com>
2026-04-20 16:15:26 -0400

    add filtering

--- internal/service/ec2/ec2_launch_template_list.go
+++ internal/service/ec2/ec2_launch_template_list.go
@@ -13,10 +13,13 @@ import (
 	awstypes "github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	"github.com/hashicorp/terraform-plugin-framework/list"
 	listschema "github.com/hashicorp/terraform-plugin-framework/list/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-provider-aws/internal/errs/fwdiag"
 	"github.com/hashicorp/terraform-provider-aws/internal/flex"
 	"github.com/hashicorp/terraform-provider-aws/internal/framework"
+	fwflex "github.com/hashicorp/terraform-provider-aws/internal/framework/flex"
+	fwtypes "github.com/hashicorp/terraform-provider-aws/internal/framework/types"
 	"github.com/hashicorp/terraform-provider-aws/internal/logging"
 	inttypes "github.com/hashicorp/terraform-provider-aws/internal/types"
 	"github.com/hashicorp/terraform-provider-aws/names"
@@ -35,9 +38,30 @@ type launchTemplateListResource struct {
 	framework.ListResourceWithSDKv2Resource
 }
 
+type listLaunchTemplateModel struct {
+	framework.WithRegionModel
+	LaunchTemplateIDs   fwtypes.ListValueOf[types.String] `tfsdk:"launch_template_ids"`
+	LaunchTemplateNames fwtypes.ListValueOf[types.String] `tfsdk:"launch_template_names"`
+	Filters             customListFilters                 `tfsdk:"filter"`
+}
+
 func (l *launchTemplateListResource) ListResourceConfigSchema(ctx context.Context, request list.ListResourceSchemaRequest, response *list.ListResourceSchemaResponse) {
 	response.Schema = listschema.Schema{
-		Attributes: map[string]listschema.Attribute{},
+		Attributes: map[string]listschema.Attribute{
+			"launch_template_ids": listschema.ListAttribute{
+				CustomType:  fwtypes.ListOfStringType,
+				ElementType: types.StringType,
+				Optional:    true,
+			},
+			"launch_template_names": listschema.ListAttribute{
+				CustomType:  fwtypes.ListOfStringType,
+				ElementType: types.StringType,
+				Optional:    true,
+			},
+		},
+		Blocks: map[string]listschema.Block{
+			names.AttrFilter: customListFiltersBlock(ctx),
+		},
 	}
 }
 
@@ -52,9 +76,13 @@ func (l *launchTemplateListResource) List(ctx context.Context, request list.List
 		}
 	}
 
-	stream.Results = func(yield func(list.ListResult) bool) {
-		input := ec2.DescribeLaunchTemplatesInput{}
+	var input ec2.DescribeLaunchTemplatesInput
+	if diags := fwflex.Expand(ctx, query, &input); diags.HasError() {
+		stream.Results = list.ListResultsStreamDiagnostics(diags)
+		return
+	}
 
+	stream.Results = func(yield func(list.ListResult) bool) {
 		for item, err := range listLaunchTemplates(ctx, conn, &input) {
 			if err != nil {
 				result := fwdiag.NewListResultErrorDiagnostic(err)
@@ -109,10 +137,6 @@ func (l *launchTemplateListResource) List(ctx context.Context, request list.List
 			}
 		}
 	}
-}
-
-type listLaunchTemplateModel struct {
-	framework.WithRegionModel
 }
 
 func listLaunchTemplates(ctx context.Context, conn *ec2.Client, input *ec2.DescribeLaunchTemplatesInput) iter.Seq2[awstypes.LaunchTemplate, error] {
