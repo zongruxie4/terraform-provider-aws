61b60f97cb2bffc81d11bbc78ecdbf86ce1cc93a
Marshall Jones <marjonm@amazon.com>
2026-04-27 16:36:12 -0500

    ci: retrigger checks

--- internal/service/securityhub/testdata/AggregatorV2/region_override/main_gen.tf
+++ internal/service/securityhub/testdata/AggregatorV2/region_override/main_gen.tf
@@ -6,6 +6,7 @@ resource "aws_securityhub_account_v2" "test" {}
 resource "aws_securityhub_aggregator_v2" "test" {
   region_linking_mode = "SPECIFIED_REGIONS"
   linked_regions      = ["us-east-1"]
+
   region = var.region
 
   depends_on = [aws_securityhub_account_v2.test]
